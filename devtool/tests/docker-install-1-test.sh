098064106b000e58cfa7dce36d9a7cb12dfe3fd6
Brad Pinter <brad.pinter@gmail.com>
2013-07-08 21:07:17 -0500

    Docker install

---
+++ devtool/tests/docker-install-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m devtool -p docker-install [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "docker-install"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
